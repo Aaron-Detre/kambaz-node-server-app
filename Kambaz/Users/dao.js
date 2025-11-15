565e7057283cc368114347682df81e8c09bcfb53
Aaron Detre <aarondetre@gmail.com>
2025-11-15 00:13:32 -0500

    sign up bug

--- Kambaz/Users/dao.js
+++ Kambaz/Users/dao.js
@@ -1,23 +1,22 @@
 import { v4 as uuidv4 } from "uuid";
 export default function UsersDao(db) {
-  let { users } = db;
   const createUser = (user) => {
     const newUser = { ...user, _id: uuidv4() };
-    db.users = [...users, newUser];
+    db.users = [...db.users, newUser];
     return newUser;
   };
-  const findAllUsers = () => users;
-  const findUserById = (userId) => users.find((user) => user._id === userId);
+  const findAllUsers = () => db.users;
+  const findUserById = (userId) => db.users.find((user) => user._id === userId);
   const findUserByUsername = (username) =>
-    users.find((user) => user.username === username);
+    db.users.find((user) => user.username === username);
   const findUserByCredentials = (username, password) =>
-    users.find(
+    db.users.find(
       (user) => user.username === username && user.password === password
     );
   const updateUser = (userId, user) =>
-    (db.users = users.map((u) => (u._id === userId ? user : u)));
+    (db.users = db.users.map((u) => (u._id === userId ? user : u)));
   const deleteUser = (userId) =>
-    (db.users = users.filter((u) => u._id !== userId));
+    (db.users = db.users.filter((u) => u._id !== userId));
   return {
     createUser,
     findAllUsers,
